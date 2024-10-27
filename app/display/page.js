1bd9e3bff3f2c453b0ac16b1d1d2c8611dd33c6c
kathryntanardy <ktanardy@gmail.com>
2024-10-27 10:44:37 -0700

    Added Classification Table

--- app/display/page.js
+++ app/display/page.js
@@ -11,13 +11,15 @@ function DisplayPage() {
           {/* Adjusted main section with flex-grow */}
           <main className={`${styles.main} flex-grow flex flex-col justify-center items-center`}>
           <h1 className="mb-4 text-l font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Your<span className="text-gray-400 dark:text-gray-300"> Expenses</span> Summarized:</h1>
-          <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Categories:</p>
+          {/* <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400"></p> */}
 
-        
+          
+
+          
           
           </main>
     
-            <CSVTable/>
+          <CSVTable/>
           {/* Footer with mt-auto to push it to the bottom */}
           <footer className="bg-white rounded-lg shadow mt-auto dark:bg-gray-800 w-full">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
